143c7f294f651771c501a05922050c60ae2e8e93
Valerii Petryniak <valerii15298@gmail.com>
2026-03-02 00:13:33 +0200

    Add loader to Route and use loader data in Index component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,8 +2,10 @@ import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
   component: Index,
+  loader: () => Promise.reject(new Error("Ooooops")),
 });
 
 function Index() {
+  Route.useLoaderData();
   return <div>Index</div>;
 }
